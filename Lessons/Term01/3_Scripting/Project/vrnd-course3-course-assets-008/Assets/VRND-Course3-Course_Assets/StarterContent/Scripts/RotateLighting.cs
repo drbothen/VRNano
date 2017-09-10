e8433df987cf3325fbc2821efd494208fba51824
Joshua Magady <josh.magady@gmail.com>
2017-09-10 13:30:57 -0500

    feat(Rollup): Repo Fix

---
+++ Lessons/Term01/3_Scripting/Project/vrnd-course3-course-assets-008/Assets/VRND-Course3-Course_Assets/StarterContent/Scripts/RotateLighting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLighting : MonoBehaviour
+{
+
+    public GameObject directionalLight;
+
+    private float startTime = 0f;
+    private bool isPressed = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+//         GvrViewer.Instance.OnTrigger += ActivateRotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+//		directionalLight.transform.Rotate(0f, Time.deltaTime, 0f);
+
+        Quaternion startRotation = Quaternion.Euler(50f, 30f, 0f);
+        Quaternion endRotation = startRotation * Quaternion.Euler(0f, 180f, 0f);
+
+	    if (isPressed == true)
+	    {
+            directionalLight.transform.rotation = Quaternion.Slerp(startRotation, endRotation, startTime / 60f);
+	        startTime += Time.deltaTime;
+	    }
+        
+	}
+
+    public void ActivateRotation()
+    {
+        isPressed = true;
+    }
+
+}
---
+++ Projects/P4/Documentaion/Persona.md
@@ -0,0 +1,10 @@
+### Persona
+
+Name: Garet Lester
+Age: 21
+Occupation: Walmart Stocker
+quote: I want everything be easy
+Motivation: I want everything in life to be easy. I dont want to use to much energy learning
+new game
+
+Vr exp: No
